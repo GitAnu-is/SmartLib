1bc485cb2254cf9fe6332cc9d2659976b00f99a1
Kavishka <kavishka@smartlib.com>
2026-04-26 09:56:24 +0530

    refactor: Simplify intent detection (Commit #18)

--- frontend/src/pages/AIAssistantPage.jsx
+++ frontend/src/pages/AIAssistantPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react'
+// Commit #18 - 2026-04-26 09:56:24
+
 // Commit #17 - 2026-04-26 09:56:23
 
 // Commit #16 - 2026-04-26 09:56:23
@@ -1994,6 +1996,7 @@ export function AIAssistantPage({ onNavigate: _onNavigate }) {
     </div>
   )
 }
+
 
 
 
