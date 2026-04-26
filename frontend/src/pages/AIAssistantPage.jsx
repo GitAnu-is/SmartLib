ed83e15084b2bacb20ce3880fa400a67612a78db
Kavishka <kavishka@smartlib.com>
2026-04-26 09:56:23 +0530

    feat: Add assistant context improvements (Commit #11)

--- frontend/src/pages/AIAssistantPage.jsx
+++ frontend/src/pages/AIAssistantPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react'
+// Commit #11 - 2026-04-26 09:56:22
+
 // Commit #10 - 2026-04-26 09:56:22
 
 // Commit #9 - 2026-04-26 09:56:22
@@ -1980,6 +1982,7 @@ export function AIAssistantPage({ onNavigate: _onNavigate }) {
     </div>
   )
 }
+
 
 
 
